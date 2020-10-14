90f903813ad72cbf1d90c85117e4c5dc2fd8df1a
kdraeder <raeder@ucar.edu>
2020-10-14 13:47:33 -0600

    mysci.py not working yet. second is good

---
+++ mysci.py
@@ -0,0 +1,17 @@
+
+# Read the data line
+# create var
+filename = "data/wxobs20170821.txt"
+# open it in read mode   w(rite) 
+datafile = open(filename, 'r')
+
+# Read first line
+print(datafile, readline())
+# Read second line
+print(datafile, readline())
+# Read third line
+print(datafile, readline())
+# Read fourth line
+print(datafile, readline())
+
+dataline.close()
