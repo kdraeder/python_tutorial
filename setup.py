568a6a956c9a1015a1a75d34de8c051979539a10
kdraeder <raeder@ucar.edu>
2021-02-10 13:46:49 -0700

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+
+from setuptools import setup
+# only used in this use case.
+
+setup(
+    name="mysci",
+    version="1.0.0",
+    description="a sample package",
+    author="kdr",
+    author_email="raeder@ucar.edu",
+    packages=["mysci"],
+    install_requires=[])
+# install_requires: Dependencies on non-default packages
